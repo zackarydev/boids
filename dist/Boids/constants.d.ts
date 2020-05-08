beb4f92e33e456b8dd2d10ba4a4ead86a64318ef
Zack Therrien <zackary.therrien@osedea.com>
2020-05-08 16:15:50 -0400

    Added github pages; removed dist files

--- dist/Boids/constants.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const BIRD_COUNT = 10;
-export declare const BIRD_WIDTH = 15;
-export declare const BIRD_HEIGHT = 12;
-export declare const BIRD_SPEED: number;
-export declare enum LayerIndex {
-    BACKGROUND = 0,
-    BIRDS = 1
-}
-//# sourceMappingURL=constants.d.ts.map
