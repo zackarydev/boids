422000197b7d3d4432f55b0366f66d57901e4c6f
Zack Therrien <zackary.therrien@osedea.com>
2020-05-08 16:13:31 -0400

    Updates

--- Boids/helpers.d.ts
+++
@@ -1,5 +0,0 @@
-import Vector2D from "./Vector2D";
-export declare const fromDegree: (degree: number) => number;
-export declare const getAngle: (vector: Vector2D) => number;
-export declare const flipVector: (vector: Vector2D, plane: "x" | "y", direction: "left" | "right" | "up" | "down") => void;
-//# sourceMappingURL=helpers.d.ts.map
